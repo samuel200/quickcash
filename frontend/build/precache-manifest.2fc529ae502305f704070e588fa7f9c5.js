858084cb0164af86d5a30df6ab1781a460a9b4ed
samuel200 <samuelemeh200@gmail.com>
2020-11-26 13:56:21 +0100

    adding build file

---
+++ frontend/build/precache-manifest.2fc529ae502305f704070e588fa7f9c5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03f6459c9e0ba10d68f68b3729fa345",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b627a93349d04b876b56",
+    "url": "/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "ca2a33c3cc60a1d44bf5",
+    "url": "/static/css/main.ebd53ae7.chunk.css"
+  },
+  {
+    "revision": "b627a93349d04b876b56",
+    "url": "/static/js/2.c0685c52.chunk.js"
+  },
+  {
+    "revision": "4daa728e75ec4b027821f08127c7af00",
+    "url": "/static/js/2.c0685c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca2a33c3cc60a1d44bf5",
+    "url": "/static/js/main.e6660f20.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "e9de8d0e510d75390a0b5f5ef312949e",
+    "url": "/static/media/active-online.e9de8d0e.jpg"
+  },
+  {
+    "revision": "5ffca2667e6e235b64e5bbf85d580203",
+    "url": "/static/media/aerial-architectural-design-architecture-buildings-373912.5ffca266.jpg"
+  },
+  {
+    "revision": "9b1414302a82e13b72d09b1761a6318d",
+    "url": "/static/media/avatar (1).9b141430.png"
+  },
+  {
+    "revision": "90602547071d9940b48988542cfe703a",
+    "url": "/static/media/discuss.90602547.png"
+  },
+  {
+    "revision": "89f18b14096878194436669283cdeff3",
+    "url": "/static/media/form-loader.89f18b14.gif"
+  },
+  {
+    "revision": "0efc892513c9fe2964d220ac1bfdbc47",
+    "url": "/static/media/game_room_background.0efc8925.jpg"
+  },
+  {
+    "revision": "b32ae22332cf46858da2b675be81e231",
+    "url": "/static/media/gameroom.b32ae223.jpg"
+  },
+  {
+    "revision": "813efcafeb8ffc70c10eb3d75db03f4a",
+    "url": "/static/media/gaming.813efcaf.png"
+  },
+  {
+    "revision": "c926907af5ad4a9485b4008600da8b63",
+    "url": "/static/media/interface (1).c926907a.png"
+  },
+  {
+    "revision": "8f7c6a38fbd6df7eaa1933fdcde72c73",
+    "url": "/static/media/interface.8f7c6a38.png"
+  },
+  {
+    "revision": "aa2e57dbf512b01626bbc1e879d45135",
+    "url": "/static/media/playing-games.aa2e57db.jpg"
+  },
+  {
+    "revision": "f34a1debe43d88b4ebb4ff006382ab41",
+    "url": "/static/media/reading-news.f34a1deb.jpg"
+  },
+  {
+    "revision": "fc6f24840af26256ce24216bef50c67d",
+    "url": "/static/media/technology (1).fc6f2484.png"
+  }
+]);
